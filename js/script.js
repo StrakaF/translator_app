9778bf709d7f50aa4a626cea6d3a0724fbf86718
FilipStraka <straka168@gmail.com>
2024-01-25 22:50:48 +0100

    selectin English by default as FROM language and Hindi as TO language

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 const selectTag = document.querySelectorAll("select");
 
-selectTag.forEach(tag => {
+selectTag.forEach((tag, id)=> {
     for( const country_code in countries ){
-        console.log(countries[country_code]);
-        let option = `<option value="${country_code}">${countries[country_code]}</option>`;
+        // selectin English by default as FROM language and Hindi as TO language
+        let selected;
+        if(id == 0 && country_code == "en-GB") {
+            selected = "selected";
+        } else if (id == 1 && country_code == "hi-IN") {
+
+        }
+        let option = `<option value="${country_code}" ${selected}>${countries[country_code]}</option>`;
         tag.insertAdjacentHTML("beforeend", option); //adding options tag inside select tag
     }
 });
