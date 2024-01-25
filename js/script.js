3b02b36e0e6dcf2d74abe69d5df7bafb323c2165
FilipStraka <straka168@gmail.com>
2024-01-25 22:41:13 +0100

    adding options tag inside select tag

--- js/script.js
+++ js/script.js
@@ -3,5 +3,7 @@ const selectTag = document.querySelectorAll("select");
 selectTag.forEach(tag => {
     for( const country_code in countries ){
         console.log(countries[country_code]);
+        let option = `<option value="${country_code}">${countries[country_code]}</option>`;
+        tag.insertAdjacentHTML("beforeend", option); //adding options tag inside select tag
     }
 });
