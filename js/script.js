cf6e83925a8accbfb35b5a014a6036b5f835be49
FilipStraka <straka168@gmail.com>
2024-01-26 22:48:41 +0100

    finished translation button and showing translated text

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 const fromText = document.querySelector(".from-text"),
+toText = document.querySelector(".to-text"),
 selectTag = document.querySelectorAll("select"),
 translateBtn = document.querySelector("button");
 
 selectTag.forEach((tag, id)=> {
     for( const country_code in countries ){
-        // selectin English by default as FROM language and Hindi as TO language
+        // selectin English by default as FROM language and Slovak as TO language
         let selected;
         if(id == 0 && country_code == "en-GB") {
             selected = "selected";
-        } else if (id == 1 && country_code == "hi-IN") {
+        } else if (id == 1 && country_code == "sk-SK") {
             selected = "selected";
         }
         let option = `<option value="${country_code}" ${selected}>${countries[country_code]}</option>`;
@@ -21,7 +22,9 @@ translateBtn.addEventListener("click", () => {
     traslateFrom = selectTag[0].value, //gettubg fromSelect tag value
     traslateTo = selectTag[1].value;    //Getting toSelect tag value
     let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${traslateFrom}|${traslateTo}`;
+    // fetching api response and returning it with parsing into js obj
+    // and in another then metod receiving that obj
     fetch(apiUrl).then(res => res.json()).then(data => {
-        console.log(data);
+        toText.value = data.responseData.translatedText;
     })
 });
