8d42d13b7348ba014355940f21101d61a1a20ac7
FilipStraka <straka168@gmail.com>
2024-01-26 22:13:34 +0100

    attach eventlistener to translate button and store text to variable text

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-const selectTag = document.querySelectorAll("select");
+const fromText = document.querySelector(".from-text"),
+selectTag = document.querySelectorAll("select"),
+translateBtn = document.querySelector("button");
 
 selectTag.forEach((tag, id)=> {
     for( const country_code in countries ){
@@ -7,9 +9,14 @@ selectTag.forEach((tag, id)=> {
         if(id == 0 && country_code == "en-GB") {
             selected = "selected";
         } else if (id == 1 && country_code == "hi-IN") {
-
+            selected = "selected";
         }
         let option = `<option value="${country_code}" ${selected}>${countries[country_code]}</option>`;
         tag.insertAdjacentHTML("beforeend", option); //adding options tag inside select tag
     }
+});
+
+translateBtn.addEventListener("click", () => {
+    let text = fromText.value;
+    console.log(text);
 });
